64944a8d03454aac2c4f17a80869de93f750e9dc
Rasmus <Rasmus.pedersen200@gmail.com>
2024-05-30 11:15:13 +0200

    aaaa

--- src/components/weatherdisplay.js
+++ src/components/weatherdisplay.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const WeatherDisplay = ({ weatherData, isCelsius, onAddFavorite }) => {
+  const [displayedTemperature, setDisplayedTemperature] = useState(null);
+
+  useEffect(() => {
+    // Konvertera temperaturen till rätt enhet när isCelsius ändras
+    if (weatherData) {
+      const temperature = isCelsius ? weatherData.temperature : celsiusToFahrenheit(weatherData.temperature);
+      setDisplayedTemperature(temperature);
+    }
+  }, [weatherData, isCelsius]);
+
+  const celsiusToFahrenheit = (celsius) => {
+    return (celsius * 9 / 5) + 32;
+  };
+
   if (!weatherData) return <p>No data available</p>;
 
   return (
-    <div class ="main">
+    <div className="main">
       <h2>{weatherData.city}</h2>
-      <p>Average temperature: {weatherData.temperature} {isCelsius ? '°C' : '°F'}</p>
+      <p>Average temperature: {displayedTemperature} {isCelsius ? '°C' : '°F'}</p>
       <button onClick={() => onAddFavorite(weatherData.city)}>Add to Favorites</button>
     </div>
   );
